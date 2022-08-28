866bc257092573d5234b38e6141621f691d5588a
Anilkumar Reddy <anilkreddy.a@gmail.com>
2022-08-28 02:52:36 -0400

    Changes input

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const inputString : string = core.getInput('name')
+    
+    core.info(inputString)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    await wait(1400)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
